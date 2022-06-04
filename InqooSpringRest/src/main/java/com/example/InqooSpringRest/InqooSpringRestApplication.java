2589791cd14b046e98217b171f6b5ed80262e967
PawelGajec <103367076+PawelGajec@users.noreply.github.com>
2022-06-04 09:36:24 +0200

    Add files via upload
    Add new project

---
+++ InqooSpringRest/src/main/java/com/example/InqooSpringRest/InqooSpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.InqooSpringRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InqooSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InqooSpringRestApplication.class, args);
+	}
+
+}
---
+++ InqooSpringRest/src/test/java/com/example/InqooSpringRest/InqooSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InqooSpringRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InqooSpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
