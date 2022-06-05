79dcc06913071e90e1acd928265429b680ac5f87
PawelGajec <103367076+PawelGajec@users.noreply.github.com>
2022-06-05 10:46:31 +0200

    Add files via upload

---
+++ another_inqoo_project/src/main/java/com/example/another_inqoo_project/AnotherInqooProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.another_inqoo_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotherInqooProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotherInqooProjectApplication.class, args);
+	}
+
+}
---
+++ another_inqoo_project/src/test/java/com/example/another_inqoo_project/AnotherInqooProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.another_inqoo_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotherInqooProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
